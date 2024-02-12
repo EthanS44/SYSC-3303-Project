080bc064d05d3ae895241eb68c883d07cd3c6f44
Sam <samwilson292929@gmail.com>
2024-02-12 16:43:53 -0500

    ELEVATOR CLASS CLOSE TO DONE

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,107 +1,55 @@
 import java.time.*;
+import java.util.ArrayList;
 
 import static java.lang.Thread.sleep;
 class ElevatorWaiting implements ElevatorState {
     @Override
     public void handle(Elevator elevator){
-        // state instructions go here
-        elevator.setState(new ElevatorMoving());
+        while(elevator.getCurrentFloor() == elevator.getNextFloor()){
+            // wait for the next floor to be different from the current one
+        }
+        elevator.setCurrentState(new ElevatorMoving());
     }
 }
-class ElevatorMoving implements ElevatorState{
+class ElevatorMoving implements ElevatorState {
     @Override
     public void handle(Elevator elevator){
+        // need to put something here to get an instruction from the instruction box
 
+        // store current floor
         int oldFloor = elevator.getCurrentFloor();
-        while(newFloor != elevator.getCurrentFloor()) {
+
+        // go to next floor
+        while(elevator.getNextFloor() != elevator.getCurrentFloor()) {
             //If statement for the elevator to go up
-            if(newFloor > elevator.getCurrentFloor()) {
-                System.out.println("Elevator " + elevator.getElevatorID() + " Going to floor " + newFloor + " Current floor " + elevator.getCurrentFloor() + "\n");
+            if(elevator.getNextFloor() > elevator.getCurrentFloor()) {
+                System.out.println("Elevator " + elevator.getElevatorID() + " Going to floor " + elevator.getNextFloor() + " Current floor " + elevator.getCurrentFloor() + "\n");
                 elevator.setCurrentFloor(elevator.getCurrentFloor() + 1);
             }
             //If statement for the elevator to go down
-            else if (newFloor < elevator.getCurrentFloor()){
-                System.out.println("Elevator " + elevator.getElevatorID() + " Going to floor " + newFloor + " Current floor " + elevator.getCurrentFloor() + "\n");
+            else if (elevator.getNextFloor() < elevator.getCurrentFloor()){
+                System.out.println("Elevator " + elevator.getElevatorID() + " Going to floor " + elevator.getNextFloor() + " Current floor " + elevator.getCurrentFloor() + "\n");
                 elevator.setCurrentFloor(elevator.getCurrentFloor() - 1);
             }
         }
         System.out.println("Arrived at floor " + elevator.getCurrentFloor());
 
-        elevator.setState(new ElevatorHandlingDoor());
+        // set state to door handling
+        elevator.setCurrentState(new ElevatorHandlingDoor());
     }
 }
 class ElevatorHandlingDoor implements ElevatorState{
     @Override
     public void handle(Elevator elevator){
-        // state instructions go here
-        elevator.setState(new ElevatorWaiting());
-    }
-}
+        // wait for elevator to be stopped
+        while(!elevator.isStopped()){
+        }
 
-public class Elevator implements Runnable {
+        // open door
+        System.out.println("Elevator " + elevator.getElevatorID() + " opening doors\n");
+        elevator.setDoorOpen(true);
 
-    private final int elevatorID;
-    private ElevatorQueue elevatorqueue;
-    private boolean stopped;
-    private int currentFloor;
-    private boolean doorOpen;
-    private ElevatorState currentState;
-
-
-    public Elevator(int elevatorID, ElevatorQueue queue){
-        this.elevatorID = elevatorID;
-        this.elevatorqueue = queue;
-        currentFloor = 1;
-        doorOpen = false;
-        this.currentState = new ElevatorWaiting();
-        System.out.println("Elevator " + elevatorID + " created\n");
-    }
-
-    public int getElevatorID() {
-        return elevatorID;
-    }
-
-    public boolean isStopped(){
-        return stopped;
-    }
-
-    public int getCurrentFloor() { return currentFloor; }
-    public void setCurrentFloor(int newFloor) { currentFloor = newFloor; }
-
-    public boolean isDoorOpen() { return doorOpen; }
-
-    public ElevatorState getCurrentState() { return currentState; }
-
-    public void pushButton(int buttonID){
-        System.out.println("Button "+buttonID+ " pushed!");
-        LocalDateTime currentTime = LocalDateTime.now();
-        //int buttonID = 5; // can be changed ofc
-        Request request = new Request(true, currentTime, elevatorID, buttonID, currentFloor);
-        elevatorqueue.putInRequestBox(request);
-        System.out.println("Request sent!");
-    }
-
-    public void goToFloor(int newFloor){
-        //int newFloor = instruction.getFloorNumber();
-    }
-
-    public void openDoor(){
-        System.out.println("Elevator " + elevatorID + " opening doors\n");
-        doorOpen = true;
-    }
-
-    public void closeDoor(){
-        System.out.println("Elevator " + elevatorID + " closing doors\n");
-        doorOpen = false;
-    }
-
-    public void handleInstruction(){
-        Instruction instructionToHandle = elevatorqueue.getFromInstructionBox();
-
-        goToFloor(instructionToHandle.getFloorNumber());
-
-        openDoor();
-
+        // sleep between open and close
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -109,24 +57,69 @@ public class Elevator implements Runnable {
             System.out.println("Failed to handle instruction" );
         }
 
-        closeDoor();
-        System.out.println("INSTRUCTION HANDLED");
-    }
+        // close door
+        System.out.println("Elevator " + elevator.getElevatorID() + " closing doors\n");
+        elevator.setDoorOpen(false);
 
-    public void setState(ElevatorState state) {
-        this.currentState = state;
+        // Sets state to waiting
+        elevator.setCurrentState(new ElevatorWaiting());
     }
+}
 
-    public void request() {
-        this.currentState.handle(this);
+public class Elevator implements Runnable {
+    private final int elevatorID;
+    private ElevatorQueue elevatorqueue;
+    private boolean stopped;
+    private int currentFloor;
+    private int nextFloor;
+    private boolean doorOpen;
+    private ElevatorState currentState;
+    private ArrayList<ElevatorButton> buttonList;
+    public Elevator(int elevatorID, ElevatorQueue queue){
+        this.elevatorID = elevatorID;
+        this.elevatorqueue = queue;
+        this.buttonList = new ArrayList<ElevatorButton>();
+        this.currentFloor = 1;
+        this.doorOpen = false;
+        this.stopped = true;
+        this.currentState = new ElevatorWaiting();
+        System.out.println("Elevator " + elevatorID + " created\n");
+
+        // adds 7 buttons to the elevator's button list
+        for (int i = 1; i <= 7; i++){
+            ElevatorButton newButton = new ElevatorButton(i);
+            addButton(newButton);
+            newButton.setElevator(this);
+        }
     }
+    public int getElevatorID() {
+        return elevatorID;
+    }
+    public boolean isStopped(){
+        return stopped;
+    }
+    public int getCurrentFloor() { return currentFloor; }
+    public void setCurrentFloor(int newFloor) { currentFloor = newFloor; }
+    public int getNextFloor() { return nextFloor; }
+    public void setNextFloor(int newFloor) {nextFloor = newFloor;}
+    public boolean isDoorOpen() { return doorOpen; }
+    public void setDoorOpen(boolean open) { doorOpen = open; }
+    public ElevatorState getCurrentState() { return currentState; }
+    public void setCurrentState(ElevatorState state) { this.currentState = state;}
+    public void addButton(ElevatorButton button){ this.buttonList.add(button);}
+    public void removeButton(ElevatorButton button){ this.buttonList.remove(button); }
+    public ArrayList<ElevatorButton> getButtonList(){ return this.buttonList; }
+    public ElevatorQueue getElevatorQueue() {return this.elevatorqueue; }
 
+    public void request() { this.currentState.handle(this);}
 
     @Override
     public void run(){
-        pushButton(5);
+        for (int i = 1; i <= 7; i++){
+            this.buttonList.get(i).pushButton();
+        }
         while(true){
-            handleInstruction();
+            request();
         }
     }
 
